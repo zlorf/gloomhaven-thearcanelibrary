381b7b9ecff8b1140a1f0ae37c2fc8df3f7cf124
Jacek Tomaszewski <jacek.tomek@gmail.com>
2019-09-08 17:01:52 +0200

    Add monster stats: Aesteher Scout/Ashblade.

--- src/constants/MonsterStats.js
+++ src/constants/MonsterStats.js
@@ -2,6 +2,351 @@
 
 export const MONSTERS = {
   "monsters": {
+    "Aesther Ashblade": {
+      "standeeCount": 4,
+      "level": [
+        {
+          "level": 0,
+          "normal": {
+            "health": 8,
+            "move": 3,
+            "attack": 2,
+            "range": 0,
+            "attributes": []
+          },
+          "elite": {
+            "health": 12,
+            "move": 3,
+            "attack": 3,
+            "range": 0,
+            "attributes": [
+              "%poison%"
+            ]
+          }
+        },
+        {
+          "level": 1,
+          "normal": {
+            "health": 9,
+            "move": 3,
+            "attack": 3,
+            "range": 0,
+            "attributes": []
+          },
+          "elite": {
+            "health": 14,
+            "move": 4,
+            "attack": 3,
+            "range": 0,
+            "attributes": [
+              "%poison%"
+            ]
+          }
+        },
+        {
+          "level": 2,
+          "normal": {
+            "health": 13,
+            "move": 3,
+            "attack": 3,
+            "range": 0,
+            "attributes": []
+          },
+          "elite": {
+            "health": 16,
+            "move": 4,
+            "attack": 4,
+            "range": 0,
+            "attributes": [
+              "%poison%"
+            ]
+          }
+        },
+        {
+          "level": 3,
+          "normal": {
+            "health": 15,
+            "move": 3,
+            "attack": 3,
+            "range": 0,
+            "attributes": [
+              "%poison%"
+            ]
+          },
+          "elite": {
+            "health": 18,
+            "move": 4,
+            "attack": 4,
+            "range": 0,
+            "attributes": [
+              "Attackers gain Disadvantage",
+              "%poison%"
+            ]
+          }
+        },
+        {
+          "level": 4,
+          "normal": {
+            "health": 15,
+            "move": 4,
+            "attack": 4,
+            "range": 0,
+            "attributes": [
+              "%poison%"
+            ]
+          },
+          "elite": {
+            "health": 21,
+            "move": 4,
+            "attack": 5,
+            "range": 0,
+            "attributes": [
+              "Attackers gain Disadvantage",
+              "%poison%"
+            ]
+          }
+        },
+        {
+          "level": 5,
+          "normal": {
+            "health": 18,
+            "move": 4,
+            "attack": 4,
+            "range": 0,
+            "attributes": [
+              "%poison%"
+            ]
+          },
+          "elite": {
+            "health": 25,
+            "move": 4,
+            "attack": 5,
+            "range": 0,
+            "attributes": [
+              "Attackers gain Disadvantage",
+              "%poison%"
+            ]
+          }
+        },
+        {
+          "level": 6,
+          "normal": {
+            "health": 21,
+            "move": 4,
+            "attack": 4,
+            "range": 0,
+            "attributes": [
+              "%poison%"
+            ]
+          },
+          "elite": {
+            "health": 28,
+            "move": 4,
+            "attack": 6,
+            "range": 0,
+            "attributes": [
+              "Attackers gain Disadvantage",
+              "%poison%"
+            ]
+          }
+        },
+        {
+          "level": 7,
+          "normal": {
+            "health": 24,
+            "move": 4,
+            "attack": 5,
+            "range": 0,
+            "attributes": [
+              "%poison%"
+            ]
+          },
+          "elite": {
+            "health": 32,
+            "move": 5,
+            "attack": 6,
+            "range": 0,
+            "attributes": [
+              "Attackers gain Disadvantage",
+              "%poison%"
+            ]
+          }
+        }
+      ]
+    },
+    "Aesther Scout": {
+      "standeeCount": 4,
+      "level": [
+        {
+          "level": 0,
+          "normal": {
+            "health": 8,
+            "move": 3,
+            "attack": 2,
+            "range": 0,
+            "attributes": []
+          },
+          "elite": {
+            "health": 13,
+            "move": 3,
+            "attack": 3,
+            "range": 0,
+            "attributes": []
+          }
+        },
+        {
+          "level": 1,
+          "normal": {
+            "health": 9,
+            "move": 3,
+            "attack": 3,
+            "range": 0,
+            "attributes": []
+          },
+          "elite": {
+            "health": 14,
+            "move": 4,
+            "attack": 3,
+            "range": 0,
+            "attributes": [
+              "%muddle%"
+            ]
+          }
+        },
+        {
+          "level": 2,
+          "normal": {
+            "health": 12,
+            "move": 3,
+            "attack": 3,
+            "range": 0,
+            "attributes": []
+          },
+          "elite": {
+            "health": 17,
+            "move": 4,
+            "attack": 3,
+            "range": 0,
+            "attributes": [
+              "%muddle%"
+            ]
+          }
+        },
+        {
+          "level": 3,
+          "normal": {
+            "health": 14,
+            "move": 4,
+            "attack": 3,
+            "range": 0,
+            "attributes": [
+              "%muddle%"
+            ]
+          },
+          "elite": {
+            "health": 19,
+            "move": 4,
+            "attack": 4,
+            "range": 0,
+            "attributes": [
+              "%muddle%",
+              "%curse%"
+            ]
+          }
+        },
+        {
+          "level": 4,
+          "normal": {
+            "health": 16,
+            "move": 4,
+            "attack": 4,
+            "range": 0,
+            "attributes": [
+              "%muddle%"
+            ]
+          },
+          "elite": {
+            "health": 23,
+            "move": 4,
+            "attack": 4,
+            "range": 0,
+            "attributes": [
+              "%muddle%",
+              "%curse%"
+            ]
+          }
+        },
+        {
+          "level": 5,
+          "normal": {
+            "health": 18,
+            "move": 4,
+            "attack": 4,
+            "range": 0,
+            "attributes": [
+              "%muddle%",
+              "%curse%"
+            ]
+          },
+          "elite": {
+            "health": 26,
+            "move": 4,
+            "attack": 5,
+            "range": 0,
+            "attributes": [
+              "%muddle%",
+              "%curse%"
+            ]
+          }
+        },
+        {
+          "level": 6,
+          "normal": {
+            "health": 21,
+            "move": 4,
+            "attack": 4,
+            "range": 0,
+            "attributes": [
+              "%muddle%",
+              "%curse%"
+            ]
+          },
+          "elite": {
+            "health": 29,
+            "move": 5,
+            "attack": 5,
+            "range": 0,
+            "attributes": [
+              "%muddle%",
+              "%curse%"
+            ]
+          }
+        },
+        {
+          "level": 7,
+          "normal": {
+            "health": 24,
+            "move": 5,
+            "attack": 4,
+            "range": 0,
+            "attributes": [
+              "%muddle%",
+              "%curse%"
+            ]
+          },
+          "elite": {
+            "health": 33,
+            "move": 5,
+            "attack": 6,
+            "range": 0,
+            "attributes": [
+              "%muddle%",
+              "%curse%"
+            ]
+          }
+        }
+      ]
+    },
     "Ancient Artillery": {
       "standeeCount": 6,
       "level": [
@@ -8233,7 +8578,6 @@ export const MONSTERS = {
         }
       ]
     }
-  
   }
 };
 
