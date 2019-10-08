2d3374c2c6e209415d04eb157091e5f3c86c65d1
Jacek Tomaszewski <jacek.tomek@gmail.com>
2019-10-09 01:26:04 +0200

    Add changelog and version number: 1.7.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,17 @@ import { Grid, Row, Col, Button } from 'react-bootstrap';
 
 const versionHistory = [
   {
+    versionNumber: "1.7",
+    items: [
+      "added missing achievement and scenario requirement from Base game",
+      "added content (scenarios, treasures, achievements, monsters) from Forgotten Circles expansion",
+      "fixed bug with prosperity changes not being saved",
+      "refactored components, cleaned files structure",
+      "added class icons to Solo scenarios",
+    ],
+    date: "2019/09/09"
+  },
+  {
     versionNumber: "1.6",
     items: [
       "local browser storage - your game will automatically return to the previous point when used in the same web browser",
@@ -94,16 +105,16 @@ class HomeComponent extends Component {
 
     return (
       <div className="container">
-      	<Grid className="home-container">
-      		<Row className="hidden-xs">
-      			<Col xs={12} md={12}>
+        <Grid className="home-container">
+          <Row className="hidden-xs">
+            <Col xs={12} md={12}>
               <div className="well">
                 <p className="lead">Welcome to <strong><em>The Arcane Library</em></strong>, a companion app and set of utilities for the board game <strong>Gloomhaven</strong>. We hope you enjoy your stay!</p>
                 <p><strong><em>The Arcane Library</em></strong> can track a variety of information about your Gloomhaven campaign, and also provides some <strong>standalone utilities</strong> that will work even when you're not tracking campaign progress.</p>
                 <p>The <strong>Save</strong> and <strong>Load</strong> buttons at the top of the page allow you to save your progress in Gloomhaven to a file in your <strong>Dropbox</strong> account. The <strong>Import</strong> and <strong>Export</strong> buttons allow you to save your progress to a text file on your computer. Be sure to save often!</p>
                 <p>If you need any help with the application, have any suggestions or requests, or have experienced any unexpected behaviour, please feel free to <strong><a href="https://boardgamegeek.com/user/FoxWithTwoTales">send me a message</a></strong> on BoardGameGeek.</p>
               </div>
-      			</Col>
+            </Col>
           </Row>
           <Row className="visible-xs">
             <Col xs={12} md={12}>
@@ -122,7 +133,7 @@ class HomeComponent extends Component {
             <Col xs={12} md={6}>
               <Button href="/utilities/scenarioLevel" bsSize="large" block className="btn-doomstalker">Scenario Level Calculator</Button>
             </Col>
-      		</Row>
+          </Row>
           <Row>
             <Col xs={12} md={12} className="text-center">
               <h3>Upcoming Features <small className="hidden-xs">What we're working on next...</small></h3>
@@ -130,7 +141,7 @@ class HomeComponent extends Component {
           </Row>
           <Row>
             <Col xs={12} md={12}>
-              <p>You can view the GitHub repository's list of bugs and new features here: <a href="https://github.com/ninjawithkillmoon/gloomhaven-thearcanelibrary/issues">Issues</a>. Feel free to comment or make requests.</p>
+              <p>You can view the GitHub repository's list of bugs and new features here: <a href="https://github.com/zlorf/gloomhaven-thearcanelibrary/issues">Issues</a>. Feel free to comment or make requests.</p>
             </Col>
           </Row>
           <Row>
@@ -146,9 +157,13 @@ class HomeComponent extends Component {
                 <li><a href="https://github.com/ninjawithkillmoon">GitHub profile</a></li>
                 <li><a href="mailto:john.william.tonkin+thearcanelibrary@gmail.com">Email</a></li>
               </ul>
-              <p>The code is available open source on <a href="https://github.com/ninjawithkillmoon/gloomhaven-thearcanelibrary">GitHub</a>. It is built using the front-end JavaScript framework <a href="https://facebook.github.io/react/">React</a>.</p>
+              <p>When Forgotten Circles expansion was released, Jacek Tomaszewski took over and added new content, refactoring and polishing things a bit on the way. It was version 1.7.</p>
+              <ul>
+                <li><a href="https://github.com/zlorf">GitHub profile</a></li>
+              </ul>
+              <p>The code is available open source on <a href="https://github.com/zlorf/gloomhaven-thearcanelibrary">GitHub</a>. It is built using the front-end JavaScript framework <a href="https://facebook.github.io/react/">React</a>.</p>
               <p>There is a Slack channel for development discussion. Message me for an invite if you would like to be involved.</p>
-              <p>Feel free to contact me regarding the app: bugs reports, feature requests, comments or offers of assistance all appreciated.</p>
+              <p>Feel free to contact me (via GitHub) regarding the app: bugs reports, feature requests, comments or offers of assistance all appreciated.</p>
               <p>None of this would be possible without the wonderful creation that is <strong><a href="https://boardgamegeek.com/boardgame/174430/gloomhaven">Gloomhaven</a></strong>, a board game by <strong><a href="https://boardgamegeek.com/boardgamedesigner/69802/isaac-childres">Isaac Childres</a></strong> at <strong><a href="http://www.cephalofair.com/">Cephalofair Games</a></strong>.</p>
             </Col>
           </Row>
